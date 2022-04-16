5cb035b483e144e941979b8b8f22d2ac990be38d
Leo <94006083+McLaner@users.noreply.github.com>
2022-04-16 16:55:24 +0300

    Ta-dam

--- outflow/settings.py
+++ outflow/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Added manually
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
